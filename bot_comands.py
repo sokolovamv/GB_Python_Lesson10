b95da4b804e8e01139a916b51e42f6e1ff4af352
Maria <masya.2506@mail.ru>
2022-11-14 17:55:33 +0300

    Change  project

--- bot_comands.py
+++ bot_comands.py
@@ -3,22 +3,44 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from spy import *
 from complex import Complex
 
-
+# Приветствие
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
     await update.message.reply_text(f'Hi {update.effective_user.first_name}!')
 
+# Команда помощь, знать какие команды есть
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
     await update.message.reply_text(f'/hi\n/help\n/rational\n/complex')
 
+# работа с рациональными числами
 async def rational_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
     msg = update.message.text
     items = msg.split()
-    x = int(items[1])
+    x = float(items[1])
     sign = items[2]
-    y = int(items[3])
+    y = float(items[3])
+    await update.message.reply_text(f'{x} {sign} {y} =  {result(x,sign,y)}')
+
+# работа с комплексными числами
+async def complex_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    log(update, context)
+    msg = update.message.text
+    items = msg.split()
+    x_Re = float(items[1])
+    if items[2] == '+':
+        x_Im = float(items[3])
+    elif items[2] == '-':
+        x_Im = -1 * float(items[3])
+    sign = items[6]
+    y_Re = float(items[7])
+    if items[8] == '+':
+        y_Im = float(items[9])
+    elif items[8] == '-':
+        y_Im = -1 * float(items[9])
+    x = Complex(x_Re,x_Im)
+    y = Complex(y_Re,y_Im)
     await update.message.reply_text(f'{x} {sign} {y} =  {result(x,sign,y)}')
 
 def result(x,sign,y):
@@ -34,22 +56,5 @@ def result(x,sign,y):
         return "Бот не понимает тебя("
 
 
-async def complex_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    log(update, context)
-    msg = update.message.text
-    items = msg.split()
-    x_Re = int(items[1])
-    if items[2] == '+':
-        x_Im = int(items[3])
-    elif items[2] == '-':
-        x_Im = -1 * int(items[3])
-    sign = items[6]
-    y_Re = int(items[7])
-    if items[8] == '+':
-        y_Im = int(items[9])
-    elif items[8] == '-':
-        y_Im = -1 * int(items[9])
-    x = Complex(x_Re,x_Im)
-    y = Complex(y_Re,y_Im)
-    await update.message.reply_text(f'{x} {sign} {y} =  {result(x,sign,y)}')
+
 
